d1acb2f7f3ebb57526d4986f061190de619d7b21
Kleinfeld, Ofri(okleinfeld) <okleinfeld@paypal.com>
2018-03-13 20:24:27 +0200

    added solution to q1c+q1d. need to write text files for these sections and progress with q1e

--- hw2/q1.py
+++ hw2/q1.py
@@ -31,35 +31,49 @@ class BreakerAssistant(object):
 
     def __init__(self):
         """Initializes BreakerAssistant object, Used to define object attribute for plaintext score"""
-        # self.english_letters = [chr(i) for i in range(ord('a'), ord('z')+1)] + [chr(i) for i in range(ord('A'), ord("Z")+1)]
-        # self.english_punc = ["!", " ", "\"", "\'", "(", ")", ",", ".", "-", "?", ":"]
-        # self.english_letter_frequency = {
-        #     'a': 0.11682, 'b': 0.04434, 'c': 0.05238, 'd': 0.03174, 'e': 0.02799, 'f': 0.04027, 'g': 0.01642,
-        #     'h': 0.042, 'i': 0.07294, 'j': 0.00511, 'k': 0.00456, 'l': 0.02415, 'm': 0.03826, 'n': 0.02284,
-        #     'o': 0.07631, 'p': 0.04319, 'q': 0.00222, 'r': 0.02826, 's': 0.06686, 't': 0.15978, 'u': 0.01183,
-        #     'v': 0.00824, 'w': 0.05497, 'x': 0.00045, 'y': 0.00763, 'z': 0.00045}
+        self.english_letters_frequency = {
+            'a': 0.08167, 'b': 0.01492, 'c': 0.02782, 'd': 0.04253, 'e': 0.12702, 'f': 0.02228, 'g': 0.02015,
+            'h': 0.06094, 'i': 0.06966, 'j': 0.00153, 'k': 0.00772, 'l': 0.04025, 'm': 0.02406, 'n': 0.06749,
+            'o': 0.07507, 'p': 0.01929, 'q': 0.00095, 'r': 0.05987, 's': 0.06327, 't': 0.09056, 'u': 0.02758,
+            'v': 0.00978, 'w': 0.0236, 'x': 0.0015, 'y': 0.01974, 'z': 0.00074}
 
-    def english_letter_proportion(self, plain_text):
+    def _text_letters_proportion(self, plain_text):
+        english_letters = [chr(i) for i in range(97,123)]
+        text_frequency = {letter: 0 for letter in english_letters}
         text_len = len(plain_text)
-        letters_count = 0
         for char in plain_text:
-            if char in self.english_letters:
-                letters_count += 1
-        letter_prop = float(letters_count) / text_len
-        return letter_prop
+            char_lowercase = char.lower()
+            if char.lower() in text_frequency:
+                text_frequency[char_lowercase] += 1
+        text_frequency = {i[0]: float(i[1])/text_len for i in text_frequency.items()}
+        return text_frequency
+
+
+    def _chi_square_statistical(self, plain_text):
+        english_freq = self.english_letters_frequency
+        text_freq = self._text_letters_proportion(plain_text)
+        # Compute chi square value - compare text distribution to expected one
+        chi_square = 0
+        for letter in english_freq.keys():
+            letter_freq = english_freq[letter]
+            text_letter_freq = text_freq[letter]
+            freq_diff = letter_freq - text_letter_freq
+            chi_square += math.pow(freq_diff, 2) / letter_freq
+        return chi_square
 
     def plaintext_score(self, plaintext):
         """Scores a candidate plaintext string, higher means more likely."""
         # Return a number (int / long / float).
         # Please don't return complex numbers, that would be just annoying.
-        raise NotImplementedError()
+        return -self._chi_square_statistical(plaintext)
 
     def brute_force(self, cipher_text, key_length):
         """Breaks a Repeated Key Cipher by brute-forcing all keys."""
         # Return a string.
-        max_score = -10000000 # need to change it
+        max_score = -100000
         plain_text = ""
         cipher = RepeatedKeyCipher()
+
         # enumerate over all possible keys using itertools
         for key in itertools.product(range(256), repeat=key_length):
             cipher.key = key
@@ -68,7 +82,7 @@ class BreakerAssistant(object):
             if dec_score > max_score:
                 max_score = dec_score
                 plain_text = dec_text
-        return plain_text
+        return plain_text.lower()
 
     def smarter_break(self, cipher_text, key_length):
         """Breaks a Repeated Key Cipher any way you like."""
@@ -76,3 +90,10 @@ class BreakerAssistant(object):
 
         raise NotImplementedError()
 
+# rp = RepeatedKeyCipher(key=[50, 179])
+# b = BreakerAssistant()
+# m = "Hello my name is Dagi. statistically this text must be long unless there is a chance the method won't work well"
+# enc = rp.encrypt(m)
+# print enc
+# dec = b.brute_force(enc, 2)
+# print dec
