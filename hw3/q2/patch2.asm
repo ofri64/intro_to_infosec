3fa4c3d60486c6a8900a052c769a799aabafcfc0
Kleinfeld, Ofri(okleinfeld) <okleinfeld@paypal.com>
2018-03-21 18:06:32 +0200

    q2 another version that for some reason doesn't change the code. need to debug using GDB

--- hw3/q2/patch2.asm
+++ hw3/q2/patch2.asm
@@ -3,11 +3,18 @@ JMP EBX
 lea eax, [ebp-0x40c]
 mov eax, [eax]
 cmp eax, 0x23
-jne label
+jne small_dead_zone_end
 lea eax, [ebp-0x40c]
 mov eax, [eax+1]
 cmp eax, 0x21
-jne label
-label:
+jne small_dead_zone_end
+lea eax, [ebp-0x40c]
+mov eax, [eax+2]
+push eax
+call 0x08048460
+pop eax
+MOV EBX, 0x08048631
+JMP EBX
+small_dead_zone_end:
 MOV EBX, 0x0804863A
 JMP EBX
