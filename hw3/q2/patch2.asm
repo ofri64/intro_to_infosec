c073808738ac2df4d77145f1bbac02a4caecb87b
Kleinfeld, Ofri(okleinfeld) <okleinfeld@paypal.com>
2018-03-21 18:05:48 +0200

    question 2 compiling version that doesnt change the flow

--- hw3/q2/patch2.asm
+++ hw3/q2/patch2.asm
@@ -1,2 +1,13 @@
-MOV EBX, 0x080485D4
+MOV EBX, 0x08048631
+JMP EBX
+lea eax, [ebp-0x40c]
+mov eax, [eax]
+cmp eax, 0x23
+jne label
+lea eax, [ebp-0x40c]
+mov eax, [eax+1]
+cmp eax, 0x21
+jne label
+label:
+MOV EBX, 0x0804863A
 JMP EBX
