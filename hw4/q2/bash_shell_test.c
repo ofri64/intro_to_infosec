2a3f292a1e16a7d9ffcde47e65fb2ef8c5300918
Kleinfeld, Ofri(okleinfeld) <okleinfeld@paypal.com>
2018-03-28 15:24:56 +0300

    adds an optional check - assembly code inside the a c code that opens a shell program

---
+++ hw4/q2/bash_shell_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+
+    asm (
+        "JMP _WANT_BIN_BASH;"
+
+        "_GOT_BIN_BASH:;"
+        "MOV EAX, 0x0B;"
+        "POP EBX;" // the RET value in our case /bin/sh
+        "XOR ECX, ECX;" // although it is not the convention, we can let argv argument to be NULL also
+        "XOR EDX, EDX;" // envp argument should be NULL
+        "INT 0x80;" // call syscall instruction
+
+        "_WANT_BIN_BASH:;"
+
+        "CALL _GOT_BIN_BASH;"
+        ".STRING \"/bin/sh\";"
+
+    );
+
+    return 0;
+}
